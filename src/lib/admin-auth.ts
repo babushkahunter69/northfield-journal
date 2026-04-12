50b1973d88c4dbdb11e11710cb2faef6cb7448dd
babushkahunter69 <babushkahunter69@gmail.com>
2026-04-12 18:18:06 +0800

    Remove stale src routes causing build failure

--- src/lib/admin-auth.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers'
-
-export async function isCookieAdmin() {
-  const cookieStore = await cookies()
-  const token = cookieStore.get('nj-admin-token')?.value
-  const adminPassword = process.env.ADMIN_PASSWORD
-
-  return Boolean(token && adminPassword && token === adminPassword)
-}
